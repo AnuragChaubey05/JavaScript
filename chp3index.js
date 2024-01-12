a3d8fa6d66965a6e6f8fb82cb9d32f5ec2f1c77b
AnuragChaubey05 <134508715+AnuragChaubey05@users.noreply.github.com>
2024-01-12 18:19:49 +0530

    Update chp3index.js

--- chp3index.js
+++ chp3index.js
@@ -5,6 +5,18 @@ let sum = 0
 let n = prompt("Enter the value of n")
 n = Number.parseInt(n)
 for (let i =0; i<n; i++){
-    sum += i+1
+    sum += (i+1);
+    // it stores sum every iteration
+   // console.log((i+1+",sum is " + sum))
 }
 console.log("Sum of first " + n + " natural number is " + sum)
+
+// Program to print table
+const n = prompt("Enter a number: ");
+for (let i=1; i <= 10; i++) {
+  console.log(i*n);
+}
+
+//Factorial
+
+
