80216c3fcb9697f960260975cb35a9eea13f1de0
AnuragChaubey05 <134508715+AnuragChaubey05@users.noreply.github.com>
2024-01-13 21:35:48 +0530

    Update chp3index.js

--- chp3index.js
+++ chp3index.js
@@ -186,6 +186,75 @@ let y = hello();
 console.log(y)
 
 
+// L- 12 PRACTICE SET ON LOOPS AND FUNCTIONS
+
+//Q1
+let marks = {
+  Hemant:100,
+  Anurag:98,
+  Ravi:70,
+  Govind:34,
+}
+
+for (let i=0; i < Object.keys(marks).length; i++){
+  console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
+ 
+// Object.keys(marks)[i] is used to get the keys of the object marks.
+// marks[Object.keys(marks)[i]] is used to get the values of the key of object marks.
+  
+}
+
+//Q2
+
+let marks = {
+  Hemant:100,
+  Anurag:98,
+  Ravi:70,
+  Govind:34,
+}
+
+for (let key in marks){
+ console.log("The marks of " + key + " are " + marks[key])
+}
+  
+//Q3
+
+let cn = 4;
+let i = prompt("Guess the number")
+console.log(typeof(i))
+while (i != cn){
+    i = prompt("Try Again");  
+}
+alert("Correct Number")
+
+// type coercion
+//Type coercion in JavaScript refers to the automatic conversion of one data type to another when certain operations are performed. 
+//JavaScript is a loosely typed language, meaning that variable types can be dynamically changed during runtime.
+
+let numString = "10";
+let result = 5 + numString; 
+// The number 5 is coerced into a string and concatenated with "10" to give "510".
+
+
+/* In JavaScript, when you use the prompt function to get input from the user, it always returns the input as a string, 
+regardless of whether the user entered a number. 
+This is why the variable i is initially a string after the user inputs their guess.
+The reason the condition i != cn works as expected is because JavaScript performs type coercion in this context. 
+When you use the != operator, JavaScript converts the operands to the same type before making the comparison. 
+In this case, it converts cn to a string and then compares the two strings.
+So, even though i is a string, the != operator converts cn to a string before making the comparison, and it works as expected.
+If you want to explicitly treat i as a number, you can use the parseInt function to convert the input string to a number. */
+
+/* It's important to note that while type coercion can be convenient, it can also lead to unexpected behavior if not used carefully. 
+To avoid potential issues, some developers prefer to use strict equality (===) and inequality (!==) operators,
+which do not perform type coercion and require both the value and the type to be the same for the comparison to be true.*/
+
+//Q4
+const mean = (a,b,c,d,e) => {
+    return (a+b+c+d+e)/5;
+} 
+
+mean(5,5,5,5,5);
 
 
 
