8975cef3c2ed4951ff6962724e4de3cf8167d747
AnuragChaubey05 <134508715+AnuragChaubey05@users.noreply.github.com>
2024-01-24 04:02:22 +0530

    Update chp7index.js

--- chp7index.js
+++ chp7index.js
@@ -79,21 +79,111 @@ console.log(arr)
 
 // L-33 Parents & Siblings of an Element
 
+// $0: Represents the currently selected DOM element in the Elements panel.
+$0 
+<button class="md:hidden text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-3 py-2">Login</button>
+
+// $1: Represents the previously selected DOM element.In other words, 
+// it's the element that was selected before the current one.
+$1
+<body cz-shortcut-listen=​"true">​…​</body>f.js">​</script>​
+ 
+// white space is act as text element
+// Notes on DOM collections:
+
+// They are read-only. This means you can't directly modify the elements in the collection,
+//but you can add or remove elements from the document using other methods.
+for ex $1 = $0 will change  the reference not the element.
+      
+// They are live.* They are live: elem.childNodes variable (reference) will automatically update if childNodes of elem is changed.   
+// They are iterable using a for...of loop. 
+
+//Siblings and the parent
+// Siblings are nodes that are children of the same parent.
+
+$1.nextElementSibling 
+$1.previousElementSibling
+      
+// for example:
+// <head> and <body> are siblings
+// Siblings share the same parent. In the above example its html
+// <body> is said to be the "next" or "right" sibling of <head>.
+// <head> is said to be the "previous" or "left" sibling of <body>.
+
+// The next sibling is in nextSibling property, and the
+// previous one in previousSibling property.
+// The parent is available as parentNode.
+
+// The parentNode method will always return the parent, but parentElement will 
+// return null  if the parent happens to be something other than an element.
+
+// alert(document.documentElement.parentNode);//document
+// alert(document.documentElement.parentNode.parentNode);// null
+ 
+
+<body>
+  <div>
+    <div class="first">first</div>
+    <div class="second">second</div>
+  </div>
+  <script>
+      console.log(document.body.firstChild)
+      a = document.body.firstChild
+      console.log(a.parentNode)
+      console.log(a.parentElement)
+      console.log(a.firstChild.nextSibling)
+  </script>
+</body>
 
 
+// L-34 Element only Navigation
+
+const changeBgRed = () => {
+  document.body.firstElementChild.style.background = "red"
+}
+
+let b = document.body
+console.log("First child of b is: ", b.firstChild)
+console.log("First Element child of b is: ", b.firstElementChild)
+
+First child of b is:  #text
+First element of b is:  nav
+
+//document.body.firstElementChild.style.background = "red"
+
+//$0.children
+HTMLCollection(3) [li, li, li]
+
+//$0.children[0].style.background = "blue";
+
+// L-35 Table Navigation 
+
+let t = document.body.firstElementChild.firstElementChild
+
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.rows)
+console.log(t.rows[0])
+console.log(t.rows[1].rowIndex)
+//run this
+console.log(tr.cells)
+console.log(tr.sectionRowIndex)
+console.log(tr.rowIndex)
+console.log(td.cellIndex)
 
 
+//quiz
+typeof document
+'object'
+typeof window
+'object'
 
-
-
-
-
-
-
-
-
-
-
+//L-36 Searching the DOM
 
 
 
