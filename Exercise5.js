c2bab9e7d85215deaaa43409eb2fe375f1da8b74
AnuragChaubey05 <134508715+AnuragChaubey05@users.noreply.github.com>
2024-04-06 15:54:52 +0530

    Update Exercise5.js

--- Exercise5.js
+++ Exercise5.js
@@ -1,8 +1,71 @@
 // L-65  Exercise 5: Hackerman
 
+async function hack() {
+   p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Initializing Hack Program");
+    }, 1000);
+  });
 
+  let p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Hacking Ravi's username");
+    }, 2000);
+  });
 
+  let p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Username found ravi_17");
+    }, 3000);
+  });
+
+  setTimeout(() => {
+    console.log("Connecting to faceboob.com");
+  }, 4000);
+  
+
+}
 
 
 
 // L-72  Exercise 5: Hackerman Solution
+
+
+let a = [
+        "Initializing Hack tool...",
+        "Connecting to Facebook...",
+        "Connecting to server 1...",
+        "Connection failed. Retrying...",
+        "Connecting to server 2",
+        "Connected Successfully...",
+        "Username iamharry...",
+        "Trying Brute Force...",
+        "200K passwords tried...",
+        "Match not found",
+        "Another 200K passwords tried...",
+        "Match not found...",
+        "Another 200K passwords tried...",
+        "Match not found...",
+        "Another 200K passwords tried...",
+        "Match found...",
+        "Accessing Account...",
+        "Hack Successful..."
+]
+
+const sleep = async (seconds) => {
+        return new Promise((resolve, reject) => {
+                setTimeout(() => { resolve(true) }, seconds * 1000)
+        })
+}
+
+const showHack = async (message) => {
+        await sleep(2)
+        // console.log(message)
+        text.innerHTML = text.innerHTML + message + "<br>"
+}
+
+(async () => {
+        for (let i = 0; i < a.length; i++) {
+                await showHack(a[i])
+        }
+})()
